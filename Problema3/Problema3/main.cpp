5b09c105e3c16e92805c10fa762849b378c16e9d
“ikrikor” <armanie94@icloud.com>
2015-10-08 12:48:55 -0500

    Change file structure and add problem 3

---
+++ Problema3/Problema3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Problema3
+//
+//  Created by Krikor Bisdikian on 10/8/15.
+//  Copyright © 2015 Krikor Bisdikian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
